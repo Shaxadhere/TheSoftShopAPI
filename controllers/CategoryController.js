b66003f674ef5b8cc59998690bd3e0f425fdb5a1
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-03 11:41:54 +0500

    Update CategoryController.js

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,32 +1,89 @@
 const Category = require('../models/CategoryModel')
 
-exports.List = (req, res) => {
-    Category.find().exec((err, data) => {
-        if(err){
-            return res.status(400).json({error: err})
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if(err || !category){
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
         }
-        return res.json({categories: data})
+        req.category = category
+        next()
     })
 }
 
-exports.View = (req, res) => {
-
+exports.read = (req, res) => {
+    return res.json({
+        success: true,
+        category: req.category
+    })
 }
 
-exports.Create = (req, res) => {
+exports.list = (req, res) => {
+    Category.find().exec((err, data) => {
+        if(err){
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
+        }
+        return res.json({
+            success: true,
+            categories: data
+        })
+    })
+}
+
+exports.create = (req, res) => {
     const category = new Category(req.body)
     category.save((err, data) => {
         if(err){
-            return res.status(400).json({error: err})
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
         }
-        res.json({category: data})
+        res.json({
+            success: true,
+            message: "Category created successfully",
+            category: data
+        })
     })
 }
 
-exports.Update = (req, res) => {
-
+exports.update = (req, res) => {
+    const category = req.category
+    category.CategoryName = req.body.CategoryName
+    category.CategorySlug = req.body.CategorySlug
+    category.CategoryTags = req.body.CategoryTags
+    category.save((err, data) => {
+        if(err){
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
+        }
+        res.json({
+            success: true,
+            message: "Category modified successfully",
+            category: data
+        })
+    })
 }
 
-exports.Delete = (req, res) => {
-
+exports.delete = (req, res) => {
+    const category = req.category
+    category.remove((err, data) => {
+        if(err){
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
+        }
+        res.json({
+            success: true,
+            message: "Category deleted successfully"
+        })
+    })
 }
