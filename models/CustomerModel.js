f32f60f818106362128b457a469c290b4df0da05
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-03 11:05:44 +0500

    authenticate methods

--- models/CustomerModel.js
+++ models/CustomerModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const crypto = require('crypto')
+const uuid = require('uuid').v1
 
 const customerSchema = new mongoose.Schema({
     FullName: {
@@ -42,5 +44,32 @@ const customerSchema = new mongoose.Schema({
         default: false,
     },
 }, {timestamps: true})
+
+customerSchema.virtual('password')
+.set(function(password){
+    this._password = password
+    this.salt = uuid()
+    this.HashedPassword = this.encryptPassword(password)
+})
+.get(function(password){
+    return this._password
+})
+
+userSchema.methods = {
+    authentiate: function(plainText){
+        return this.encryptPassword(plainText) === this.HashedPassword
+    },
+    encryptPassword: function(password){
+        if(!password) return ''
+        try{
+            return crypto.createHmac('sha1', this.salt)
+            .update(password),
+            digest('hex')
+        }
+        catch (err) {
+            return ''
+        }
+    }
+}
 
 module.exports = mongoose.model("Customer", customerSchema)
--- models/UserModel.js
+++ models/UserModel.js
@@ -40,4 +40,33 @@ const userSchema = new mongoose.Schema({
     }
 }, {timestamps: true})
 
+//Virtual Password Field
+userSchema.virtual('password')
+.set(function(password){
+    this._password = password
+    this.salt = uuid()
+    this.HashedPassword = this.encryptPassword(password)
+})
+.get(function(password){
+    return this._password
+})
+
+//model methods
+userSchema.methods = {
+    authenticate: function(plainText){
+        return this.encryptPassword(plainText) === this.HashedPassword
+    },
+    encryptPassword: function(password){
+        if(!password) return ''
+        try{
+            return crypto.createHmac('sha1', this.salt)
+            .update(password)
+            .digest('hex')
+        }
+        catch (err){
+            return ""
+        }
+    }
+}
+
 module.exports = mongoose.model("User", userSchema)
