daa54f08c7524f6ee8c0c966d1defa176244338a
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-03 13:02:02 +0500

    Update SizeController.js

--- controllers/SizeController.js
+++ controllers/SizeController.js
@@ -13,9 +13,75 @@ exports.sizeById = (req, res, next, id) => {
     })
 }
 
-exports.read(req, res) => {
+exports.read = (req, res) => {
     return res.json({
         success: true,
         size: req.size
+    })
+}
+
+exports.create = (req, res) => {
+    const size = new Size(req.body)
+    size.save((err, data) => {
+        if(err){
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
+        }
+        res.json({
+            success: true,
+            message: "Size created successfully",
+            size: size
+        })
+    })
+}
+
+exports.list = (req, res) => {
+    Size.find().exec((err, data) => {
+        if(err){
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
+        }
+        res.json({
+            succcess: true,
+            sizes: data
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    const size = req.size
+    size.SizeValue = req.body.SizeValue
+    size.save((err, data) => {
+        if(err){
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
+        }
+        res.json({
+            success: true,
+            message: "Size updated successfully",
+            size: data
+        })
+    })
+}
+
+exports.delete = (req, res) => {
+    const size = req.size
+    size.remove((err, data) => {
+        if(err) {
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
+        }
+        res.json({
+            success: true,
+            message: "Size deleted successfully"
+        })
     })
 }
