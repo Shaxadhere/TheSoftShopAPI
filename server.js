97e441118202437236e52cad9f06a9e101a0d20e
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-03 09:49:32 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const expressValidator = require('express-validator')
+require('dotenv').config()
+
+//app
+const app = express()
+
+//database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(() => console.log(`Connected to database`))
+
