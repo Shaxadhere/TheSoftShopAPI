69b6e7aeee5a53c0b49bdd033a4a658556cf2a8a
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-03 10:09:27 +0500

    Create ProductModel.js

---
+++ models/ProductModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema
+
+const productSchema = new mongoose.Schema({
+    ProductName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100,
+    },
+    Price:{
+        type: Number,
+        required: true,
+    },
+    ProductSlug:{
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100,
+    },
+    ProductDescription:{
+        type: String,
+        required: false,
+        trim: true,
+        maxlength: 1000,
+    },
+    Categories:{
+        type: Array,
+        default: [],
+    },
+    ProductTags:{
+        type: Array,
+        default: [],
+    },
+    ProductImages:{
+        type: Array,
+        default: [],
+    },
+    Status:{
+        type: Boolean,
+        default: true,
+    },
+    Deleted:{
+
+    },
+})
+
+module.exports = mongoose.model("Product", productSchema)
