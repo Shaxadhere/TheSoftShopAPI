d5172af728a21fe6c52b9511404ed80d18ca7cae
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-05 11:16:11 +0500

    Update CustomerRoutes.js

--- routes/CustomerRoutes.js
+++ routes/CustomerRoutes.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const router = express.Router()
+const {list, read, create, update, remove, customerById} = require('../controllers/CustomerController')
+
+router.get('/', list)
+router.get('/:customerId', read)
+router.post('/create', create)
+router.put('/:customerId', update)
+router.delete('/:customerId', remove)
+
+router.param('customerId', customerById)
+
+module.exports = router
