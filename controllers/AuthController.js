d72b635053217355444312e7f36d4bbfd117aa22
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-06 11:39:15 +0500

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,1 +1,9 @@
 const Customer = require('../models/CustomerModel')
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
+require('dotenv').config()
+
+exports.signupCustomer = (req, res) => {
+    const customer = new Customer(req.body)
+    
+}
